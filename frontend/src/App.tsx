c66cd3fbca5c16be0ef21a238a5dad57585168f2
Anburaj <anburaj2203@gmail.com>
2023-10-06 19:59:21 +0530

    created basic setup for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import Footer from './components/Footer';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
+import logo from "../images/buddy1.png"
+import { Link } from "react-router-dom";
+// frontend\src\images\logo.PNG
+
+export const Navbar: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+
+    if(isDarkMode){
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+    else{
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    }
+    // document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <nav
+      className={`flex items-center justify-between py-2 px-8 h-50 ${
+        isDarkMode ? "text-white bg-[#254b61]" : "text-black bg-[white]"
+      }  sticky top-0 z-10`}
+    >
+      <Link to={'/'}>
+      <div className="text-xl font-bold flex justify-center items-center">
+        <img className="w-80" src={logo} alt="Logo" />{" "}
+        {/* Use the imported logo */}
+      </div>
+      </Link>
+      <div>
+        <button className="px-2 py-1 rounded" onClick={toggleTheme}>
+          {isDarkMode ? <FiSun size={18} /> : <FiMoon size={18} />}
+        </button>
+      </div>
+    </nav>
+  );
+};
